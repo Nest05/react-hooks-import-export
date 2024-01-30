98ad34b1470eadb356aadd44646c3f53acd101ff
Nestor Masinde <nestormasinde@gmail.com>
2024-01-30 20:29:05 +0300

    Import_Export Successful!

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,18 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import { trees, wildlife, elevation }from "./parks/RockyMountain";
+
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
-
-  return <h1>Colorado State Parks!</h1>;
+  console.log(trees);
+  wildlife();
+  elevation();
+  return <div> 
+        <h1>Colorado State Parks!</h1>
+        <MesaVerde />    
+    </div>
 }
+
+export default ColoradoStateParks
